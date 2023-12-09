d23218b44fb19378d4e5b725d936e0dfb9a92f41
Kitsune424 <60034144+Kitsune424@users.noreply.github.com>
2023-12-09 21:41:21 +0300

    REBUILDING

--- Assets/scripts/UI/Crosshair.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class Crosshair : MonoBehaviour
-{
-    private RectTransform reticle;
-}
